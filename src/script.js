f83a369fcac5817cea8b225423acd75ff34a93b9
Alexandre Leitão Santos <alexandre.leitao.santos@gmail.com>
2022-05-02 20:31:23 +0200

    update controls

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 import './style.css'
 import * as THREE from 'three'
-// import { ArcballControls } from 'three/examples/jsm/controls/ArcballControls.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
