cae4382670553316cdb48fa7c313f5c127992657
Alexandre Leitão Santos <alexandre.leitao.santos@gmail.com>
2022-05-12 16:39:24 +0200

    Uptdates

--- src/script.js
+++ src/script.js
@@ -1,17 +1,23 @@
-import * as THREE from 'three'; Object
+import * as THREE from 'three'; import { Vector3 } from 'three';
+Object
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer';
 
 import './style.css';
 
-let camera, scene, controls, renderer, directionalLight, earth, earthRadius, id, place, sitesListSize, css2DRenderer;
+let camera, scene, controls, renderer, directionalLight, earth, sun, earthRadius, id, place, sitesListSize, css2DRenderer;
 
 const clock = new THREE.Clock();
 const textureLoader = new THREE.TextureLoader();
 const sites = {};
 
-const sizes = { width: window.innerWidth, height: window.innerHeight, };
+const canvas = document.querySelector( '.navigation' )
+
+const sizes = {
+    width: canvas.offsetWidth,
+    height: canvas.offsetHeight,
+};
 
 init();
 animate();
@@ -22,15 +28,15 @@ function init() {
     scene = new THREE.Scene()
 
     // Initial camera
-    camera = new THREE.PerspectiveCamera( 60, sizes.width / sizes.height, 0.01, 10 );
+    camera = new THREE.PerspectiveCamera( 60, sizes.width / sizes.height, 0.01, 20 );
     camera.position.set( 2.5, 0, 0 );
     scene.add( camera );
 
     // Lights
-    const ambientLight = new THREE.AmbientLight( 0xffcccc, 1 );
+    const ambientLight = new THREE.AmbientLight( 0xffcccc, 0.5 );
 
-    directionalLight = new THREE.DirectionalLight( 0xccffff, 2 );
-    directionalLight.position.set( 0, 1.25, 2.5 )
+    directionalLight = new THREE.DirectionalLight( 0xccffff, 0.5 );
+    directionalLight.position.set( 0, 5, 10 )
 
     scene.add( ambientLight, directionalLight );
 
@@ -40,7 +46,7 @@ function init() {
     earthRadius = 1;
     const earthGeometry = new THREE.SphereGeometry( earthRadius, 180, 90 );
     const earthMaterial = new THREE.MeshStandardMaterial( {
-        color: '#444',
+        // color: '#444',
         map: textureLoader.load( '/textures/earth/8k_earth_map.png' ),
         roughness: 0.5,
         roughnessMap: textureLoader.load( '/textures/earth/8k_earth_specular.png' ),
@@ -52,6 +58,14 @@ function init() {
     } );
     earth = new THREE.Mesh( earthGeometry, earthMaterial );
     scene.add( earth );
+
+
+    // Sun
+    const sunGeometry = new THREE.SphereGeometry( 0.1, 180, 90 );
+    const sunMaterial = new THREE.MeshBasicMaterial( { color: '#fff' } )
+    sun = new THREE.Mesh( sunGeometry, sunMaterial );
+    sun.position.set( 0, 5, 10 )
+    scene.add( sun );
 
     // Sites
     place = new THREE.Vector3();
@@ -71,6 +85,7 @@ function init() {
                 sites[ id ].site = row.getElementsByTagName( 'site' )[ 0 ].innerHTML;
                 sites[ id ].category = row.getElementsByTagName( 'category' )[ 0 ].innerHTML;
                 sites[ id ].dateInscribed = Number( row.getElementsByTagName( 'date_inscribed' )[ 0 ].innerHTML );
+                sites[ id ].danger = Boolean( row.getElementsByTagName( 'danger' )[ 0 ].innerHTML );
                 sites[ id ].latitude = Number( row.getElementsByTagName( 'latitude' )[ 0 ].innerHTML );
                 sites[ id ].longitude = Number( row.getElementsByTagName( 'longitude' )[ 0 ].innerHTML );
                 sites[ id ].phi = ( ( -sites[ id ][ 'latitude' ] + 90 ) / 180 ) * Math.PI;
@@ -78,21 +93,9 @@ function init() {
 
                 place.setFromSphericalCoords( earthRadius, sites[ id ].phi, sites[ id ].theta );
 
-                // sites[ id ][ 'siteGeometry' ] = new THREE.PlaneGeometry( 0.005, 0.005 );
-                // sites[ id ][ 'siteMaterial' ] = new THREE.MeshBasicMaterial( {
-                //     side: THREE.DoubleSide,
-                //     transparent: true,
-                //     alphaMap: textureLoader.load( '/textures/place.png' ),
-                //     depthWrite: false,
-                // } );
-
-                // sites[ id ][ 'siteMesh' ] = new THREE.Mesh( sites[ id ][ 'siteGeometry' ], sites[ id ][ 'siteMaterial' ] );
-                // sites[ id ][ 'siteMesh' ].position.set( place.x, place.y, place.z );
-                // sites[ id ][ 'siteMesh' ].lookAt( 0, 0, 0 );
-                // scene.add( sites[ id ][ 'siteMesh' ] );
-
                 sites[ id ].labelDiv = document.createElement( 'div' );
-                sites[ id ].labelDiv.className = 'label';
+                sites[ id ].labelDiv.className = 'label tooltip';
+                sites[ id ].labelDiv.addEventListener( 'click', () => { console.log( 'click' ) } );
                 sites[ id ].label = new CSS2DObject( sites[ id ].labelDiv );
                 sites[ id ].label.position.set( place.x, place.y, place.z );
                 earth.add( sites[ id ].label );
@@ -100,18 +103,18 @@ function init() {
                 switch ( sites[ id ].category ) {
                     case 'Natural':
                         // sites[ id ][ 'siteMaterial' ].color = new THREE.Color( 0x00ff00 );
-                        sites[ id ].labelDiv.innerHTML = '<i class="fa-solid fa-tree"></i>';
-                        sites[ id ].labelDiv.style.color = '#b7b7a4'
+                        sites[ id ].labelDiv.innerHTML = `<i class="fa-solid fa-tree"></i><span class="tooltiptext">${ stringToHTML( sites[ id ].site ).innerText }</span>`;
+                        sites[ id ].labelDiv.style.color = ( sites[ id ].danger ? '#b88181' : '#b7b7a4' )
                         break;
                     case 'Cultural':
                         // sites[ id ][ 'siteMaterial' ].color = new THREE.Color( 0xff0000 );
-                        sites[ id ].labelDiv.innerHTML = '<i class="fa-solid fa-landmark"></i>';
-                        sites[ id ].labelDiv.style.color = '#ddbea9'
+                        sites[ id ].labelDiv.innerHTML = `<i class="fa-solid fa-landmark"></i><span class="tooltiptext">${ stringToHTML( sites[ id ].site ).innerText }</span>`;
+                        sites[ id ].labelDiv.style.color = ( sites[ id ].danger ? '#b88181' : '#ddbea9' )
                         break;
                     case 'Mixed':
                         // sites[ id ][ 'siteMaterial' ].color = new THREE.Color( 0xffff00 );
-                        sites[ id ].labelDiv.innerHTML = '<i class="fa-solid fa-circle-plus"></i>';
-                        sites[ id ].labelDiv.style.color = '#ffe8d6'
+                        sites[ id ].labelDiv.innerHTML = `<i class="fa-solid fa-circle-plus"></i><span class="tooltiptext">${ stringToHTML( sites[ id ].site ).innerText }</span>`;
+                        sites[ id ].labelDiv.style.color = ( sites[ id ].danger ? '#b88181' : '#ffe8d6' )
                         break;
                 }
             }
@@ -129,7 +132,7 @@ function init() {
     renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2 ) );
     renderer.setSize( sizes.width, sizes.height );
     renderer.domElement.style.zIndex = '0';
-    document.body.appendChild( renderer.domElement );
+    canvas.appendChild( renderer.domElement );
 
     // CSS2D Renderer
     css2DRenderer = new CSS2DRenderer();
@@ -137,7 +140,7 @@ function init() {
     css2DRenderer.domElement.style.position = 'absolute';
     css2DRenderer.domElement.style.top = '0px';
     css2DRenderer.domElement.style.zIndex = '1';
-    document.body.appendChild( css2DRenderer.domElement );
+    canvas.appendChild( css2DRenderer.domElement );
 
 
     // Controls
@@ -151,7 +154,7 @@ function init() {
     controls.maxDistance = 5;
     controls.minDistance = 1.10;
     controls.rotateSpeed = Math.sqrt( earth.position.distanceTo( camera.position ) - 1 );
-    window.addEventListener(
+    canvas.addEventListener(
         'click',
         () => { controls.autoRotate = false },
         { once: true }
@@ -163,10 +166,12 @@ function init() {
 
 };
 
-function labelsDistanceTest() {
+function labelsUpdate() {
     Object.keys( sites ).forEach( id => {
         if ( sites[ id ].label.position.distanceTo( camera.position ) >
-            Math.sqrt( ( earth.position.distanceTo( camera.position ) ** 2 ) - ( earthRadius ** 2 ) ) ) {
+            Math.sqrt( ( earth.position.distanceTo( camera.position ) ** 2 ) - ( earthRadius ** 2 ) )
+            || sites[ id ].dateInscribed > slider.value
+        ) {
             sites[ id ].labelDiv.style.visibility = 'hidden'
         } else {
             sites[ id ].labelDiv.style.visibility = 'visible'
@@ -179,15 +184,15 @@ function labelsDistanceTest() {
 function onWindowResize() {
 
     // Update sizes
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
+    sizes.width = canvas.offsetWidth,
+        sizes.height = canvas.offsetHeight,
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height;
+        // Update camera
+        camera.aspect = sizes.width / sizes.height;
     camera.updateProjectionMatrix();
 
     // Update Labels
-    labelsDistanceTest()
+    labelsUpdate()
 
     // Update Renderers
     renderer.setSize( sizes.width, sizes.height );
@@ -207,15 +212,43 @@ function animate() {
     controls.rotateSpeed = Math.sqrt( earth.position.distanceTo( camera.position ) - 1 );
 
     // Update Labels
-    labelsDistanceTest()
+    labelsUpdate()
 
     // Update objects
-    directionalLight.position.z = Math.sin( ( elapsedTime + 25 ) / 25 ) * 2.5
-    directionalLight.position.x = Math.cos( ( elapsedTime + 25 ) / 25 ) * 2.5
+    directionalLight.position.z = Math.sin( ( elapsedTime + 25 ) / 25 ) * 10
+    directionalLight.position.x = Math.cos( ( elapsedTime + 25 ) / 25 ) * 10
+    // directionalLight.lookAt( earth.position )
+
+    sun.position.z = Math.sin( ( elapsedTime + 25 ) / 25 ) * 10
+    sun.position.x = Math.cos( ( elapsedTime + 25 ) / 25 ) * 10
 
     // Update Renderers
     renderer.render( scene, camera );
     css2DRenderer.render( scene, camera );
 
 }
+
+// Current Year
+let currentYear = new Date().getFullYear();
+document.getElementById( "currentYear" ).innerHTML = currentYear;
+
+// Slider
+var slider = document.getElementById( "yearRange" );
+var output = document.getElementById( "demo" );
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
+
+// Site Interaction
+function siteClick() {
+    console.log( 'clicked' )
+}
+
+function stringToHTML( str ) {
+    var dom = document.createElement( 'div' );
+    dom.innerHTML = str;
+    return dom;
+};
 
